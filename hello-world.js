47176132fc166aec68dfb2e23b924e8f4f55118d
Cristian Barillas <cristianvx@yahoo.com>
2018-09-30 17:02:11 -0600

    index file

--- hello-world.js
+++ hello-world.js
@@ -3,12 +3,17 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
+const server = http.createServer((request, response) => {
+    console.log("A user made a request" + request.url);
+    response.writeHead(200, {"Context-Type": "text/plain"});
+    response.write("Here is some data");
+    response.end();
 });
 
-server.listen(port, hostname, () => {
+//http.createServer(onRequestMethod).listen(3000);
+server.listen(port, hostname, (err) => {
+    if(err){
+        return console.log('something bad happened', err)
+    }
     console.log(`Server running at http://${hostname}:${port}/`);
 });
